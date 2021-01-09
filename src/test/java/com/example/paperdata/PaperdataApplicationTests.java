e486c10fa87668f9f0ac107fa6b36db80a9894fe
CheneyZZZ <154593855@qq.com>
2021-01-09 14:36:18 +0800

    spring framework

---
+++ src/test/java/com/example/paperdata/PaperdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paperdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaperdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
